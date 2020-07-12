e901a278c1dbb8071fdece6541abd97ff4de3009
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-12 15:24:05 -0400

    Created javascript for storage, included getNotes function

---
+++ Develop/db/store.js
@@ -0,0 +1,33 @@
+const util = require("util");
+const fs = require("fs");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+  constructor() {
+    this.lastId = 0;
+  }
+
+  read() {
+    return readFileAsync("db/db.json", "utf8");
+  }
+
+  write(note) {
+    return writeFileAsync("db/db.json", JSON.stringify(note));
+  }
+
+  getNotes() {
+    return this.read().then(notes => {
+      let parsedNotes;
+
+      // If notes isn't an array or can't be turned into one, send back a new empty array
+      try {
+        parsedNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        parsedNotes = [];
+      }
+
+      return parsedNotes;
+    });
+  }
