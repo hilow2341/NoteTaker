7edada1ed5a382b35def206c4d14f9edd4dd5611
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-12 15:34:32 -0400

    Added ability to remove a note, finalized store.js

--- Develop/db/store.js
+++ Develop/db/store.js
@@ -21,7 +21,6 @@ class Store {
     return this.read().then(notes => {
       let parsedNotes;
 
-      // If notes isn't an array or can't be turned into one, send back a new empty array
       try {
         parsedNotes = [].concat(JSON.parse(notes));
       } catch (err) {
@@ -39,12 +38,20 @@ class Store {
       throw new Error("Note 'title' and 'text' cannot be blank");
     }
 
-    // Increment `this.lastId` and assign it to `newNote.id`
     const newNote = { title, text, id: ++this.lastId };
 
-    // Get all notes, add the new note, write all the updated notes, return the newNote
     return this.getNotes()
       .then(notes => [...notes, newNote])
       .then(updatedNotes => this.write(updatedNotes))
       .then(() => newNote);
   }
+
+  removeNote(id) {
+
+    return this.getNotes()
+      .then(notes => notes.filter(note => note.id !== parseInt(id)))
+      .then(filteredNotes => this.write(filteredNotes));
+  }
+}
+
+module.exports = new Store();
