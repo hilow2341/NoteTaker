e1615dbc9646c6c3743cba3babc65ba5bfc4a9aa
hilow2341 <60238749+hilow2341@users.noreply.github.com>
2020-07-12 15:04:04 -0400

    Added a post request to my API routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -8,3 +8,10 @@ router.get("/notes", function(req, res) {
     .then(notes => res.json(notes))
     .catch(err => res.status(500).json(err));
 });
+
+router.post("/notes", (req, res) => {
+    store
+      .addNote(req.body)
+      .then((note) => res.json(note))
+      .catch(err => res.status(500).json(err));
+  });
